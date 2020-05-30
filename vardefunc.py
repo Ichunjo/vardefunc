bfcaf2cff0df417479dbb7efffc431728777f6a9
Ichunjo <ichunjo.le.terrible@gmail.com>
2020-05-30 15:21:25 +0200

    Update generate_keyframes
    Now use scxvid and wwxd for better accuracy

--- vardefunc.py
+++ vardefunc.py
@@ -179,20 +179,18 @@ def get_chroma_shift(src_h: int = None, dst_h: int = None,
     ch_shift = float(round(ch_shift, 5))
     return ch_shift
 
-def generate_keyframes(clip: vs.VideoNode, out_path: str = None,
-                       use_scxvid: bool = True) -> NoReturn:
+def generate_keyframes(clip: vs.VideoNode, out_path: str = None) -> NoReturn:
     """
     Generate qp filename for keyframes for timing or pass the file into the encoder
-    to force I frames. Stolen from the kagefunc.
+    to force I frames. Use both scxvid and wwxd. Original function stolen from kagefunc.
     """
     clip = core.resize.Bilinear(clip, 640, 360)
-    if use_scxvid:
-        clip = core.scxvid.Scxvid(clip)
-    else:
-        clip = core.wwxd.WWXD(clip)
+    clip = core.scxvid.Scxvid(clip)
+    clip = core.wwxd.WWXD(clip)
     out_txt = ""
     for i in range(clip.num_frames):
-        if clip.get_frame(i).props["_SceneChangePrev"] == 1:
+        if clip.get_frame(i).props["_SceneChangePrev"] == 1 \
+            or clip.get_frame(i).props["Scenechange"] == 1:
             out_txt += "%d I -1\n" % i
         if i % 500 == 0:
             print(i)
