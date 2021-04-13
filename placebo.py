adbdd451373212f1b7871521f013b98019d29a88
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-04-13 14:57:45 +0200

    Update placebo.deband function

--- placebo.py
+++ placebo.py
@@ -7,29 +7,76 @@ import vapoursynth as vs
 core = vs.core
 
 
-def deband(clip: vs.VideoNode, radius: int = 17, threshold: float = 4,
-           iterations: int = 1, grain: float = 4, chroma: bool = True)-> vs.VideoNode:
+def deband(clip: vs.VideoNode, radius: float = 16.0,
+           threshold: Union[float, List[float]] = 4.0, iterations: int = 1,
+           grain: Union[int, List[int]] = 4, chroma: bool = True, **kwargs)-> vs.VideoNode:
     """Wrapper for placebo.Deband
 
     Args:
-        clip (vs.VideoNode):
-        radius (int, optional): Defaults to 17.
-        threshold (float, optional): Defaults to 4.
-        iterations (int, optional): Defaults to 1.
-        grain (float, optional): Defaults to 4.
-        chroma (bool, optional): Defaults to True.
+        clip (vs.VideoNode): Source clip.
+
+        radius (int, optional):
+            The debanding filter's initial radius. The radius increases linearly for each iteration.
+            A higher radius will find more gradients, but a lower radius will smooth more aggressively.
+            Defaults to 16.0.
+
+        threshold (Union[float, List[float]], optional):
+            The debanding filter's cut-off threshold.
+            Higher numbers increase the debanding strength dramatically,
+            but progressively diminish image details. Defaults to 4.0.
+
+        iterations (int, optional):
+            The number of debanding steps to perform per sample. Each step reduces a bit more banding,
+            but takes time to compute. Note that the strength of each step falls off very quickly,
+            so high numbers (>4) are practically useless. Defaults to 1.
+
+        grain (Union[int, List[int]], optional):
+            Add some extra noise to the image.
+            This significantly helps cover up remaining quantization artifacts.
+            Higher numbers add more noise.
+
+            Note: When debanding HDR sources, even a small amount of grain can result
+            in a very big change to the brightness level.
+            It's recommended to either scale this value down or disable it entirely for HDR.
+            Defaults to 6.0, which is very mild.
+
+        chroma (bool, optional): Process chroma planes or not. Defaults to True.
 
     Returns:
-        vs.VideoNode
+        vs.VideoNode: Debanded clip.
     """
-    if get_depth(clip) != 16:
-        clip = depth(clip, 16)
-    if chroma is True:
-        clip = join([core.placebo.Deband(x, 1, iterations, threshold, radius, grain)
-                     for x in split(clip)])
+    if chroma:
+        if isinstance(threshold, int):
+            threshold = [cast(int, threshold)]
+
+        threshold = cast(List[int], threshold)
+
+        while len(threshold) < 3:
+            threshold.append(threshold[len(threshold) - 1])
+
+
+        if isinstance(grain, int):
+            grain = [cast(int, grain)]
+
+        grain = cast(List[int], grain)
+
+        while len(grain) < 2:
+            grain.append(grain[len(grain) - 1])
+
+
+        values = dict(zip(threshold, grain))
+
+        planes = split(clip)
+
+        for i, (thr, gra) in enumerate(values.items()):
+            planes[i] = planes[i].placebo.Deband(1, iterations, thr, radius, gra, **kwargs)
+        clip = join(planes)
     else:
-        clip = core.placebo.Deband(clip, 1, iterations, threshold, radius, grain)
+        clip = clip.placebo.Deband(1, iterations, threshold, radius, grain, **kwargs)
+
     return clip
+
+
 
 def shader(clip: vs.VideoNode, width: int, height: int, shader_file: str, luma_only: bool = True, **args)-> vs.VideoNode:
     """Wrapper for placebo.Resample
