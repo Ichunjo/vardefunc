0b93c8af2122b65b04201c0dc5875dc7e94ab648
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-05-03 04:05:51 +0200

    Squashed commit of the following:
    commit 072797558534505326a21b79e8c10f234aeea833
    Author: Riven Skaye <14993038+RivenSkaye@users.noreply.github.com>
    Date:   Sat May 1 13:56:59 2021 +0200
    
        Fixed docstring width
    
    commit 49728d0609b9cf0510adc5331c73d8b39d3491ae
    Author: RivenSkaye <pretender0906@gmail.com>
    Date:   Sat May 1 13:54:10 2021 +0200
    
        Added a check to not process multiple planes when there's only one

--- placebo.py
+++ placebo.py
@@ -40,12 +40,13 @@ def deband(clip: vs.VideoNode, radius: float = 16.0,
             It's recommended to either scale this value down or disable it entirely for HDR.
             Defaults to 6.0, which is very mild.
 
-        chroma (bool, optional): Process chroma planes or not. Defaults to True.
+        chroma (bool, optional): Process chroma planes or not.
+            Defaults to True if the input clip has chroma planes.
 
     Returns:
         vs.VideoNode: Debanded clip.
     """
-    if chroma:
+    if chroma and clip.format.num_planes > 1:
         if isinstance(threshold, (float, int)):
             threshold = [cast(float, threshold)]
 
