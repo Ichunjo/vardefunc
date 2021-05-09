4d5a1c198f709bcf1784f5adcce68f922d9d64ca
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-05-09 04:14:46 +0200

    util: Add load_operators_expr and max_expr function

--- vardefunc/util.py
+++ vardefunc/util.py
@@ -1,3 +1,5 @@
+"""Helper functions for the main functions in the script."""
+from string import ascii_lowercase
 import vapoursynth as vs
 
 core = vs.core
@@ -13,3 +15,27 @@ def get_sample_type(clip: vs.VideoNode) -> vs.SampleType:
         [type]: [description]
     """
     return clip.format.sample_type
+
+
+def load_operators_expr() -> str:
+    """[summary]
+
+    Returns:
+        str: [description]
+    """
+    abcd = list(ascii_lowercase)
+    return abcd[-3:] + abcd[:-3]
+
+
+def max_expr(n: int) -> str:
+    """Dynamic max string to be integrated in std.Expr.
+
+    Args:
+        n (int): Number of elements.
+
+    Returns:
+        str: Expression.
+    """
+    return 'x y max ' + ' max '.join(
+        load_operators_expr()[i] for i in range(2, n)
+    ) + ' max'
