6df59797d3d3d99cf71737606760e6e5407d34e3
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-05-17 02:21:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,30 @@ from setuptools import setup
 
 
 
-requirements = open('requirements.txt').read()
+requirements = open("requirements.txt").read()
 
 
-name = 'vardefunc'
-version = '1.0'
+NAME = "vardefunc"
+VERSION = "1.0"
 
 setup(
-    name=name,
-    version=version,
-    author='Vardë',
+    name=NAME,
+    version=VERSION,
+    author="Vardë",
     author_email="ichunjo.le.terrible@gmail.com",
     description="Vardë's Vapoursynth functions",
-    long_description='README.md',
+    long_description="README.md",
     url="https://github.com/Ichunjo/vardefunc",
-    packages=['vardefunc'],
+    download_url="https://github.com/Ichunjo/vardefunc/archive/refs/tags/v1.0-beta.zip",
+    packages=["vardefunc"],
     install_requires=requirements,
-    python_requires='>=3.8',
-    zip_safe=False
+    python_requires=">=3.8",
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
