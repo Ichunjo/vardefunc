bd6753f8b2c3d5930ed99b9fb2562dc130ecc91e
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-05-24 16:09:29 +0200

    util: sort functions

--- vardefunc/util.py
+++ vardefunc/util.py
@@ -7,8 +7,30 @@ import vapoursynth as vs
 core = vs.core
 
 
+class FormatError(Exception):
+    """Raised when a format of VideoNode object is not allowed."""
+    pass  # noqa: PLW0107
+
+
+def copy_docstring_from(source: Callable[..., Any]) -> Callable[..., Any]:
+    """Decorator intended to copy the docstring from an other function
+
+    Args:
+        source (Callable[..., Any]): Source function.
+
+    Returns:
+        Callable[..., Any]: Function decorated
+    """
+    def wrapper(func: Callable[..., Any]) -> Callable[..., Any]:
+        func.__doc__ = source.__doc__
+        return func
+    return wrapper
+
+
 def get_sample_type(clip: vs.VideoNode) -> vs.SampleType:
     """Returns the sample type of a VideoNode as an SampleType."""
+    if clip.format is None:
+        raise FormatError('Variable format not allowed!')
     return clip.format.sample_type
 
 
@@ -16,20 +38,6 @@ def load_operators_expr() -> List[str]:
     """Returns clip loads operators for std.Expr as a list of string."""
     abcd = list(ascii_lowercase)
     return abcd[-3:] + abcd[:-3]
-
-
-def max_expr(n: int) -> str:  # noqa: PLC0103
-    """Dynamic variable max string to be integrated in std.Expr.
-
-    Args:
-        n (int): Number of elements.
-
-    Returns:
-        str: Expression.
-    """
-    return 'x y max ' + ' max '.join(
-        load_operators_expr()[i] for i in range(2, n)
-    ) + ' max'
 
 
 def mae_expr(gray_only: bool = True) -> str:
@@ -46,6 +54,20 @@ def mae_expr(gray_only: bool = True) -> str:
     return 'x y - abs' if gray_only else 'x a - abs y b - abs max z c - abs max'
 
 
+def max_expr(n: int) -> str:  # noqa: PLC0103
+    """Dynamic variable max string to be integrated in std.Expr.
+
+    Args:
+        n (int): Number of elements.
+
+    Returns:
+        str: Expression.
+    """
+    return 'x y max ' + ' max '.join(
+        load_operators_expr()[i] for i in range(2, n)
+    ) + ' max'
+
+
 def rmse_expr(gray_only: bool = True) -> str:
     """Root Mean Squared Error string to be integrated in std.Expr.
 
@@ -58,18 +80,3 @@ def rmse_expr(gray_only: bool = True) -> str:
         str: Expression.
     """
     return 'x y - dup * sqrt' if gray_only else 'x a - dup * sqrt y b - dup * sqrt max z c - dup * sqrt max'
-
-
-def copy_docstring_from(source: Callable[..., Any]) -> Callable[..., Any]:
-    """Decorator intended to copy the docstring from an other function
-
-    Args:
-        source (Callable[..., Any]): Source function.
-
-    Returns:
-        Callable[..., Any]: Function decorated
-    """
-    def wrapper(func: Callable[..., Any]) -> Callable[..., Any]:
-        func.__doc__ = source.__doc__
-        return func
-    return wrapper
