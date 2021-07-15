d90524bb0f7f5460402dccffe952cd405309831d
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-07-15 04:45:05 +0200

    add types to explicit exports

--- vardefunc/__init__.py
+++ vardefunc/__init__.py
@@ -2,7 +2,7 @@
 
 # flake8: noqa
 
-from . import deband, mask, misc, noise, placebo, scale, sharp, util
+from . import deband, mask, misc, noise, placebo, scale, sharp, types, util
 
 drm = mask.Difference().rescale
 dcm = mask.Difference().creditless
