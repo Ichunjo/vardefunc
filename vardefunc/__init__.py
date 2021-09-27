b80096ab0c2a6a6149c9386405cb3826b260ab66
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-09-27 02:59:59 +0200

    fix missing import

--- vardefunc/__init__.py
+++ vardefunc/__init__.py
@@ -2,7 +2,7 @@
 
 # flake8: noqa
 
-from . import deband, mask, misc, noise, placebo, scale, sharp, types, util
+from . import aa, deband, mask, misc, noise, placebo, scale, sharp, types, util
 
 drm = mask.Difference().rescale
 dcm = mask.Difference().creditless
