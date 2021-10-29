4c8d9963c202ac1914aaa0a25599912d1c598e39
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-10-29 22:44:28 +0200

    types: add samples conversion

--- vardefunc/types.py
+++ vardefunc/types.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from enum import Enum, IntEnum
+from fractions import Fraction
 from os import PathLike
 from typing import (Any, Callable, Dict, List, Literal, Optional, Sequence,
                     Tuple, TypeVar, Union, cast)
@@ -9,6 +10,7 @@ from numpy import array as np_array
 from numpy import int8, int16, int32, uint8, uint16, uint32
 from numpy.lib.index_tricks import CClass as NP_CClass
 from numpy.typing import NDArray
+from pytimeconv import Convert
 from vapoursynth import VideoFormat, VideoNode
 from vsutil import Dither
 
@@ -249,6 +251,9 @@ class DuplicateFrame(int):
         df = super().__new__(cls, x)
         df.dup = dup
         return df
+
+    def to_samples(self, ref_fps: Fraction, sample_rate: int) -> DuplicateFrame:
+        return DuplicateFrame(Convert.f2samples(int(self), ref_fps, sample_rate), dup=self.dup)
 
     def __repr__(self) -> str:
         return f'<DuplicateFrame object: \'x:{super().__repr__()}, dup:{self.dup}\'>'
