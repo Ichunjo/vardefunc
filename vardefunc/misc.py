48938db9aedc00221e9173ce202584c549a98f2b
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-12-09 18:10:28 +0100

    misc: add thresholding function

--- vardefunc/misc.py
+++ vardefunc/misc.py
@@ -8,7 +8,8 @@ from functools import partial, wraps
 from itertools import count
 from operator import ilshift, imatmul, ior
 from typing import (Any, Callable, ClassVar, Dict, Iterable, Iterator, List,
-                    MutableMapping, Optional, Tuple, Union, cast, overload)
+                    MutableMapping, NamedTuple, Optional, Sequence, Tuple,
+                    Union, cast, overload)
 
 import vapoursynth as vs
 from lvsfunc.comparison import Stack
@@ -281,6 +282,89 @@ class DebugOutput(DebugOutputMMap):
                 if output.alpha:
                     warnings.warn('DebugOutput: VideoOutputTuple.alpha detected; this is not supported', Warning)
         return outputs
+
+
+class Thresholds(NamedTuple):
+    """
+    [soft_bound_min, [hard_bound_min, hard_bound_max], soft_bound_max)
+    """
+    clip: vs.VideoNode
+    soft_bound_min: int | float | Sequence[int] | Sequence[float]
+    hard_bound_min: int | float | Sequence[int] | Sequence[float]
+    hard_bound_max: int | float | Sequence[int] | Sequence[float]
+    soft_bound_max: int | float | Sequence[int] | Sequence[float]
+    coef_min: int | float | Sequence[int] | Sequence[float] | None = None
+    coef_max: int | float | Sequence[int] | Sequence[float] | None = None
+
+
+def thresholding(*thrs: Thresholds, base: Optional[vs.VideoNode] = None, guidance: Optional[vs.VideoNode] = None) -> vs.VideoNode:
+    """
+    General function for applying specific filtering on specific thresholds
+    with gradation support before and after the hard thresholds
+
+    Args:
+        thrs (Thresholds):
+            Positional arguments of Thresholds.
+
+        base (vs.VideoNode, optional):
+            Base clip on which the first application will be made.
+            If not specified, a blank clip is made from the first ``thrs``.
+
+        guidance (VideoNode, optional):
+            Guidance clip on which the threshold references are taken.
+            If not specified, the guidance clip is made from the first ``thrs``.
+
+    Returns:
+        vs.VideoNode:
+            Thresholded clip.
+    """
+    if not base:
+        base = thrs[0].clip.std.BlankClip()
+    if not guidance:
+        guidance = thrs[0].clip
+
+    if not base.format or not guidance.format:
+        raise ValueError('thresholding: variable format not allowed')
+
+    for i, thr in enumerate(thrs):
+        if thr.clip.format != base.format:
+            raise ValueError(f'thresholding: threshold {i} has a different format than base clip')
+
+    def _normalise_thr(thr: int | float | Sequence[int] | Sequence[float], num_planes: int) -> List[int | float]:
+        thr = [thr] if isinstance(thr, (float, int)) else thr
+        return (list(thr) + [thr[-1]] * (num_planes - len(thr)))[:num_planes]
+
+    pclip = base
+
+    for thr in thrs:
+        soft_bound_min, hard_bound_min, hard_bound_max, soft_bound_max = (_normalise_thr(t, base.format.num_planes) for t in thr[1:5])
+        coef_min = _normalise_thr(thr.coef_min, base.format.num_planes) if thr.coef_min else None
+        coef_max = _normalise_thr(thr.coef_max, base.format.num_planes) if thr.coef_max else None
+
+        exprs: List[str] = []
+        for i in range(base.format.num_planes):
+            if_in_min = f'x {soft_bound_min[i]} >= x {hard_bound_min[i]} < and'
+            if_in_max = f'x {hard_bound_max[i]} >= x {soft_bound_max[i]} < and'
+            if_in_hard = f'x {hard_bound_min[i]} >= x {hard_bound_max[i]} < and'
+
+            str_min = f'x {soft_bound_min[i]} - {hard_bound_min[i]} {soft_bound_min[i]} - /'
+            if coef_min:
+                str_min += f' {coef_min[i]} pow'
+
+            str_max = f'x {hard_bound_max[i]} - {soft_bound_max[i]} {hard_bound_max[i]} - /'
+            if coef_max:
+                str_max += f' {coef_max} pow'
+
+            exprs.append(
+                if_in_min + f' z {str_min} * y 1 {str_min} - * + '
+                + if_in_max + f' y {str_max} * z 1 {str_max} - * + '
+                + if_in_hard + ' z y ? ? ?'
+            )
+
+        pclip = core.std.Expr([guidance, pclip, thr.clip], exprs)
+
+    return pclip
+
 
 
 def fade_filter(clip: vs.VideoNode, clip_a: vs.VideoNode, clip_b: vs.VideoNode,
